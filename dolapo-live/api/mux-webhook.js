09d815c9bc97310dad9eecaed809cbea6a12e1aa
iamadesims-hash <iamadedolaposims@gmail.com>
2026-05-25 21:08:23 -0700

    Update mux-webhook.js

--- dolapo-live/api/mux-webhook.js
+++ dolapo-live/api/mux-webhook.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
   try {
     const { createClient } = await import('@supabase/supabase-js');
     const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL=https://txdbvsocyokbhozpertx.supabase.co,
+      process.env.YOUR_SUPABASE_URL,
       process.env.SUPABASE_SERVICE_ROLE_KEY
     );
 
